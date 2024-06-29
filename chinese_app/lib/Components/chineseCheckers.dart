75fa596e5a9bdce9bf61eca0019d54fbabfc24b7
pablolopez247 <pablo.lopez02@intouchcx.com>
2024-06-28 22:37:43 -0600

    Adding chinese checkers file and first level to chinese project

---
+++ chinese_app/lib/Components/chineseCheckers.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ChineseCheckers extends StatefulWidget {
+  const ChineseCheckers({super.key});
+
+  @override
+  State<ChineseCheckers> createState() => _ChineseCheckersState();
+}
+
+class _ChineseCheckersState extends State<ChineseCheckers> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ chinese_app/lib/Levels/level1.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Level1 extends StatelessWidget {
+  const Level1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
